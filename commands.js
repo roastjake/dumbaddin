153b620d4fd0795e21d8720297a8f014b1fbfb50
roastjake <96138187+roastjake@users.noreply.github.com>
2024-04-25 14:58:35 -0400

    Add files via upload

---
+++ commands.js
@@ -0,0 +1,4 @@
+function openBrowserWindow() {
+    // Your code here. For example:
+    window.open('https://forms.office.com/Pages/ResponsePage.aspx?id=JsaDXAreBES8VJBqeCyWJe59I0hRwUhIlBdU-tcRzylUNEdBSU5XNDdRUlhPRDY4RURDQVJSNlg0Uy4u');
+}
